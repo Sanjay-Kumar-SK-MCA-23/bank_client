5c1ce5c7a63c0d4eb627d80f77cfd977764c0c1d
Sanjay Kumar M <139735109+Sanjay-Kumar-SK-MCA-23@users.noreply.github.com>
2024-03-21 14:00:51 +0530

    Add files via upload

---
+++ src/home.js
@@ -0,0 +1,13 @@
+export default function Home(){
+    return(
+        <>
+        <div id="home">
+         <h3>Welcome!<span id="span-1">Tamilnadu banking</span>-<span id="span-2">Net-Banking</span></h3>
+         <h4>"We have onlone banking service!"</h4>
+         <p> 24 hours banking service</p>
+          <p>Open new deposit, withdrawal account through our bank service via online</p>
+
+        </div>  
+        </>
+    )
+}
